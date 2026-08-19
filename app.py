c74b704645cfd29a82f58348ac80814fd1740e8f
Yagna Gurram <yagnagurram629@gmail.com>
2026-08-19 15:19:02 +0530

    Clean up todo app

--- app.py
+++ app.py
@@ -5,23 +5,12 @@ todos=[]
 
 @app.route('/', methods=['GET','POST'])
 def home():
-    name="Red"
-    age=22
-    names=["Red","John","Alice","Bob"]
-    todo=""
+    
     if request.method == 'POST':
         todo = request.form['todo']
         todos.append(todo)
-        return render_template('home.html',name=name,age=age,names=names,todos=todos)
-    return render_template('home.html',name=name,age=age,names=names,todos=todos)
-
-@app.route('/about')
-def about():
-    return "I am learning flask"
-
-@app.route('/contact')
-def contact():
-    return "Contact me at:"
+        
+    return render_template('home.html',todos=todos)
 
 if __name__=='__main__':
     app.run(debug=True)
