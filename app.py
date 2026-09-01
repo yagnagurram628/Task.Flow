09beae737beb0e7f566b43f68d153474f20afb49
Yagna Gurram <yagnagurram628@gmail.com>
2026-09-01 15:06:13 +0530

    added delete, complete, undo buttons to tasks

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 app = Flask(__name__)
 
 todos=[]
@@ -7,10 +7,23 @@ todos=[]
 def home():
     
     if request.method == 'POST':
-        todo = request.form['todo']
+        todo = {
+            "text":request.form['todo'],
+            "completed":False
+        }
         todos.append(todo)
         
     return render_template('home.html',todos=todos)
+
+@app.route('/delete/<int:index>')
+def delete(index):
+    todos.pop(index)
+    return redirect(url_for('home'))
+
+@app.route('/completed/<int:index>')
+def completed(index):
+    todos[index]["completed"]=not todos[index]["completed"]
+    return redirect(url_for('home'))
 
 if __name__=='__main__':
     app.run(debug=True)
