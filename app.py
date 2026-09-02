3dfe84b0aedb8896588da2a2a3d4600599e36a9c
Yagna Gurram <yagnagurram628@gmail.com>
2026-09-02 14:52:55 +0530

    Added edit task,empty field validation in both main and in edit

--- app.py
+++ app.py
@@ -7,8 +7,13 @@ todos=[]
 def home():
     
     if request.method == 'POST':
+        todo=request.form['todo']
+
+        if not todo:
+            return render_template('home.html',todos=todos,error="Field is empty!")
+        
         todo = {
-            "text":request.form['todo'],
+            "text":todo ,
             "completed":False
         }
         todos.append(todo)
@@ -24,6 +29,16 @@ def delete(index):
 def completed(index):
     todos[index]["completed"]=not todos[index]["completed"]
     return redirect(url_for('home'))
+
+@app.route('/edit/<int:index>',methods=['GET','POST'])
+def edit(index):
+    if request.method=="POST":
+        ntext=request.form['text']
+        if not ntext:
+            return render_template('edit.html',text=ntext, error="Field is empty!")
+        todos[index]["text"]=ntext
+        return redirect(url_for('home'))
+    return render_template('edit.html',text=todos[index]["text"])
 
 if __name__=='__main__':
     app.run(debug=True)
